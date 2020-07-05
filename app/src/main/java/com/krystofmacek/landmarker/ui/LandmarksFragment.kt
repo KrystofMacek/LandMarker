5817ab0f3de5063a40baf0ef4ceb404f095bd959
Krystof <krystofmacek333@gmail.com>
2020-07-05 21:01:49 +0200

    Initial commit

---
+++ app/src/main/java/com/krystofmacek/landmarker/ui/LandmarksFragment.kt
@@ -0,0 +1,24 @@
+package com.krystofmacek.landmarker.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.krystofmacek.landmarker.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LandmarksFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_landmarks, container, false)
+    }
+
+}
