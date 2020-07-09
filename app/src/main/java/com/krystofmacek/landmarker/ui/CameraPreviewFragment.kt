acd508baad475bc1e2ae1b13a7f1812f6154c533
Krystof <krystofmacek333@gmail.com>
2020-07-09 10:29:30 +0200

    Implemented CameraX CaptureImage use case

--- app/src/main/java/com/krystofmacek/landmarker/ui/CameraPreviewFragment.kt
+++ app/src/main/java/com/krystofmacek/landmarker/ui/CameraPreviewFragment.kt
@@ -2,23 +2,26 @@ package com.krystofmacek.landmarker.ui
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import java.nio.ByteBuffer
 import com.krystofmacek.landmarker.R
 import kotlinx.android.synthetic.main.fragment_camera_preview.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -28,10 +31,17 @@ class CameraPreviewFragment : Fragment() {
     private var preview: Preview? = null
     private var camera: Camera? = null
 
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_camera_preview, container, false)
+        val btnCapture = view.findViewById<Button>(R.id.preview_button_capture)
+        btnCapture.setOnClickListener { takePhoto() }
 
         // Request camera permissions
         if (allPermissionsGranted()) {
@@ -41,8 +51,11 @@ class CameraPreviewFragment : Fragment() {
                 requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
+        outputDirectory = getOutputDirectory()
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera_preview, container, false)
+        return view
     }
 
     override fun onRequestPermissionsResult(
@@ -70,13 +83,14 @@ class CameraPreviewFragment : Fragment() {
             val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
 
             preview = Preview.Builder().build()
+            imageCapture = ImageCapture.Builder().build()
 
             val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
 
             try {
                 cameraProvider.unbindAll()
 
-                camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview)
+                camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview, imageCapture)
                 preview?.setSurfaceProvider(
                     viewFinder.createSurfaceProvider()
                 )
@@ -88,8 +102,48 @@ class CameraPreviewFragment : Fragment() {
     }
 
     private fun takePhoto() {
-        // TODO
+        // get reference to imageCapture use case, if its null return out of the function
+        // this will be null if you tap the photo button before image capture is setup
+        // without returning it would just crash
+        val imageCapture = imageCapture ?: return
+
+        // create a file to hold the image. Add in a time stamp so the file name will be unique.
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+                .format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        // Create an OutputFileOptions object. This object is where you can specify things about how you want your output to be.
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        // Call takePicture() on the imageCapture object. Pass in outputOptions, the executor, and a callback for when the image is saved.
+        imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(context), object : ImageCapture.OnImageSavedCallback{
+
+            // Save the photo to the file you created earlier, present a toast to let the user know it was successful, and print a log statement.
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Photo saved at $savedUri"
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                Log.d(TAG, msg)
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+            }
+
+        })
     }
+
+
+
+    fun getOutputDirectory(): File {
+        val mediaDir = requireContext().externalMediaDirs.firstOrNull().let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir.exists())
+            mediaDir else requireContext().filesDir
+    }
+
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat
             .checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
